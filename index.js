042a89ee0502a727f9dd0034a3169e15467f2da2
kingheedo <52102550+kingheedo@users.noreply.github.com>
2020-04-16 14:46:32 +0900

    2020-04-16
    2020-04-16

---
+++ index.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('.header');
+header.addEventListener('click', () => {
+                      header.classList.toggle('active');
+                      });
+
+const slideImage = document.querySelectorAll('.slide img');
